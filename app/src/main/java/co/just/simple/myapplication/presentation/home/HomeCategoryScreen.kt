805b48441b842f575a23c89f038c8df3e6209cdd
Aung Bo Bo Phyoe <aung.phyoe@lomatechnology.com>
2024-05-03 01:28:45 +0700

    wip

---
+++ app/src/main/java/co/just/simple/myapplication/presentation/home/HomeCategoryScreen.kt
@@ -0,0 +1,23 @@
+package co.just.simple.myapplication.presentation.home
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+@Composable
+fun HomeCategoryScreen( navHostController: NavHostController) {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+    }
+}
+
+@Composable
+fun MainCategoryItem() {
+    Column {
+
+    }
+}
